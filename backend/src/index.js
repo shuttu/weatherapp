9e8c98deb42dad1499c0df24b7bc57b7d5c58648
shuttu <36275635+shuttu@users.noreply.github.com>
2018-02-11 16:14:37 +0200

    second commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || 'a7168658152cd2224f656b07235c62e2';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
